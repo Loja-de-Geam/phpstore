dc18ca3d0bb4b2f934f26326ee07fae12f24044b
Mateus Soares da Rocha Cordeiro <msgdhdjf777@gmail.com>
2023-07-16 15:52:25 -0300

    inicio da criação do index do site

--- core/views/inicio.php
+++ core/views/inicio.php
@@ -4,10 +4,107 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= APP_NAME?></title>
-    <link rel="shortcut icon" href="assets/images/logo/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="estilo/inicio.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,0,200" />
+    <link rel="stylesheet" href="..\..\public\assets\css\inicio.css">
+    <link rel="shortcut icon" href="assets\images\logo\favicon.ico" type="image/x-icon">
+    <title>Início</title>
 </head>
 <body>
-    <h1>OI</h1>
+    <header>
+        <nav class="navegador">
+            <img src="assets\images\logo\LOGO.png" alt="" width="50px" height="50px">
+            <a href="#" class="titulo">Fynder Foodie</a>
+            <ul class="nav">
+                <li class="nav-item"><a href="#">Home</a></li>
+                <li class="nav-item"><a href="#">About</a></li>
+                <li class="nav-item"><a href="#">Menu</a></li>
+                <li class="nav-item"><a href="#">Contact</a></li>
+                <li class="nav-item">
+                    <button onclick="window.location.href='login.html'" class="botaoEC">Entrar</button>
+                    <button onclick="window.location.href='cadastro.html'" class="botaoEC">Cadastrar</button>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="cinza"></div>
+            <div class="Vermelho1"><button class="bb-expandir"><span class="material-symbols-outlined">expand_more</span></button></div>
+            <div class="Amarelo1"><button class="bb-expandir"><span class="material-symbols-outlined">expand_more</span></button></div>
+            <div class="Amarelo2"><button class="bb-expandir"><span class="material-symbols-outlined">expand_more</span></button></div>
+            <div class="Vermelho2"><button class="bb-expandir"><span class="material-symbols-outlined">expand_more</span></button></div>
+            <div class="Vermelho3"><button class="bb-expandir"><span class="material-symbols-outlined">expand_more</span></button></div>
+            <div class="Amarelo3"><button class="bb-expandir"><span class="material-symbols-outlined">expand_more</span></button></div>
+        </div>
+    </main>
+    <!--==============================================================================================-->
+    <footer>
+        <div class="rodape">
+            <div class="boxs">
+                <img src="assets\images\logo\LOGO.png" alt="" width="70px">
+            </div>
+            <div class="boxs">
+                <h2>Páginas</h2> <br>
+                <ul>
+                    <li><a href="comidas">Home</a></li>
+                    <li><a href="/suporte">Suporte</a></li>
+                </ul>
+            </div>
+            <div class="boxs"> 
+                <h2>Sobre nós</h2> <br>
+                <ul>
+                    <li><a href="/termos">Sobre a empresa</a></li>
+                    <li><a href="#">O que fazemos</a></li>
+                </ul>
+            </div>
+            <div class="boxs">
+                <h2>Contatos</h2> <br>
+                <ul>
+                    <li><a href="@" target="_blank">GitHub</a></li>
+                    <li><a href="@" target="_blank">WhatsApp</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="copy">
+            <p>&copy;2022 Copyright - Fynder Foodie</p>
+        </div>
+    </footer>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+        const buttons = document.querySelectorAll('.bb-expandir');
+        const divs = document.querySelectorAll('.Vermelho1, .Vermelho2, .Vermelho3, .Amarelo1, .Amarelo2, .Amarelo3');
+
+        buttons.forEach((button, index) => {
+            button.addEventListener('click', function() {
+            const div = divs[index];
+            divs.forEach((divItem, divIndex) => {
+                if (divIndex === index) {
+                divItem.classList.toggle('expandir');
+                } else {
+                divItem.classList.remove('expandir');
+                }
+            });
+            });
+        });
+        });
+
+    </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+          const buttons = document.querySelectorAll('.bb-expandir');
+      
+          buttons.forEach((button) => {
+            button.addEventListener('click', function() {
+              const span = button.querySelector('span.material-symbols-outlined');
+              if (span.textContent === 'expand_more') {
+                span.textContent = 'expand_less';
+              } else {
+                span.textContent = 'expand_more';
+              }
+            });
+          });
+        });
+      </script>
 </body>
 </html>
