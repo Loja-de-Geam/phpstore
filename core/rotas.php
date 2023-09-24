a8579f7876fda43500bdbaa5816fd4b5ce15b5f3
Gabriel Kleber <gabrielkleber54321n@gmail.com>
2023-09-24 17:08:50 -0300

    .

--- core/rotas.php
+++ core/rotas.php
@@ -16,7 +16,7 @@
         'erro' => 'main@erro',
         'logout' => 'main@logout',
         'adm' => 'main@adm',
-        'cadastroComidas' => 'main@cadComidas',
+        'cadComidas' => 'main@cadComidas',
         'edit' => 'main@edit',
         'delete' => 'main@delete',
         'comidas' => 'main@comidas'
