b274425c3280f230b3f06ec09ebab2a626abb673
Mateus Soares da Rocha Cordeiro <msgdhdjf777@gmail.com>
2023-11-08 16:07:59 -0300

    melhora na edição de produtos

--- core/views/edit.php
+++ core/views/edit.php
@@ -9,42 +9,62 @@ if (!isset($_SESSION['adm'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=".css">
     <title>Editar dados da Comida</title>
     <style>
-        * {
+        @charset "UTF-8";
+
+            @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+        
+            * {
             padding: 0;
             margin: 0;
             box-sizing: border-box;
-            font-family: Arial, Helvetica, sans-serif;
+            font-family: 'Inter', sans-serif;
         }
 
-        main {
-            width: 100vw;
+        body {
+            width: 100%;
             height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
             background-image: linear-gradient(to bottom, #002366, #004080);
         }
 
         .conteiner {
-            width: 100%;
-            height: 100%;
-            display: grid;
-            place-items: center;
+            width: 60%;
+            height: 85vh;
+            display: flex;
+            box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.308);
+            border-radius: 7px;
         }
 
-        form {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 20px;
-            height: 600px;
-            width: 500px;
+        .form {
+            width: 100%;
             display: flex;
             flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            background-color: #fff;
+            padding: 3rem;
+            border-radius: 7px;
+        }
+
+        
+
+        .grupo_input {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            padding: 0.3rem 0;
         }
 
         .caixa {
             display: flex;
             flex-direction: column;
-            margin: 20px;
+            margin-bottom: 1.1rem;
         }
 
         .caixa input,
@@ -54,7 +74,18 @@ if (!isset($_SESSION['adm'])) {
             border: none;
             resize: none;
             border-radius: 10px;
-            box-shadow: 1px 1px 6px #000000aa;
+            box-shadow: 1px 1px 6px #0000007c;
+
+        }
+
+        .caixa input:hover,
+        .caixa textarea:hover {
+            background-color: #eeeeee75;
+        }
+
+        .caixa input:focus-visible,
+        .caixa textarea:focus-visible {
+            outline: 1px solid #6c63ff;
         }
 
         .caixa label {
@@ -64,77 +95,85 @@ if (!isset($_SESSION['adm'])) {
             color: #000000;
         }
 
-        .caixa input:hover {
-            background-color: #eeeeee75;
-        }
-
-        .caixa input:focus-visible {
-            outline: 1px solid #9B59B6;
+        .caixa input::placeholder {
+            color: #000000be;
         }
 
         button {
             width: 100%;
-            height: 40px;
+            margin-top: 0.5rem;
             border: none;
             background-color: #423acf;
-            /* Cor principal */
-            padding: .4rem .7rem;
+            padding: 0.62rem;
             border-radius: 20px;
             cursor: pointer;
-            color: white;
-            font-weight: bold;
-            transition: .8s;
-            margin-bottom: 10px;
+            color: #fff;
+            font-size: 0.93rem;
+            font-weight: bolder;
         }
 
         button:hover {
             color: #423acf;
-            /* Cor principal */
-            border: 2px solid #423acf;
-            /* Cor principal */
             background-color: #fff;
-            margin-right: 5px;
-            box-shadow: 0px 0px 10px #0000006b;
+            border: 1px solid #423acf;
+            padding: 0.56rem;
         }
 
-        form h1 {
+        .voltar {
+            color: #000;
+            text-decoration: none;
+            padding-top: 5px;
+            font-size: 1.19em;
+            transition: .8s;
+        }
+
+        .voltar:hover {
+            color: #423acf;
+            font-weight: bold;
+            text-decoration: underline;
+        }
+        .form h1 {
             margin-top: 0;
-            text-align: center;
+            font-size: 3rem;
             color: #09072e;
-            margin-bottom: 1rem;
+            margin-bottom: 3rem;
         }
     </style>
 </head>
 
 <body>
-    <main>
-        <div class="conteiner">
-
+    
+    <div class="conteiner">
+        <div class="form">
+            <h1>Editar</h1>
             <form action="" method="post">
-                <h1>Editar</h1>
-                <div class="caixa">
-                    <label for="nome">Nome </label>
-                    <input type="text" name="nome" id="nome" placeholder="Nome do produto" required>
+            
+                <div class="grupo_input">
+                    <div class="caixa">
+                        <label for="nome">Nome </label>
+                        <input type="text" name="nome" id="nome" placeholder="Nome do produto" required>
+                    </div>
+                    <div class="caixa">
+                        <label for="preco">Preço </label>
+                        <input type="number" id="preco" name="preco" step="0.01" placeholder="Preço do produto" required>
+                    </div>
+                    <div class="caixa">
+                        <label for="descricao">Descrição Curta</label>
+                        <input type="text" name="descricao" id="descricao" maxlength="50" placeholder="Descrição curta">
+                    </div>
+                    <div class="caixa">
+                        <label for="descricao">Descrição para o "Saiba Mais"</label><br>
+                        <textarea name="descricao_saiba_mais" id="descricao_saiba_mais" cols="30" rows="10"></textarea>
+                    </div>
                 </div>
-                <div class="caixa">
-                    <label for="preco">Preço </label>
-                    <input type="number" id="preco" name="preco" step="0.01" placeholder="Preço do produto" required>
-                </div>
-                <div class="caixa">
-                    <label for="descricao">Descrição Curta</label><br>
-                    <input type="text" name="descricao" id="descricao" maxlength="50">
-                </div>
-                <div class="caixa">
-                    <label for="descricao">Descrição para o "Saiba Mais"</label><br>
-                    <textarea name="descricao_saiba_mais" id="descricao_saiba_mais" cols="30" rows="10"></textarea>
-                </div>
-                <div class="caixa">
-                    <button name="update">Enviar</button>
-                    <button class="voltar" onclick="window.location.href='./?a=comidas'">Voltar</button>
-                </div>
+                <div class="enviar">
+                        <button name="update">Enviar</button>
+                    </div>
             </form>
+            <a class="voltar" onclick="window.location.href='./?a=comidas'">Voltar</a>
         </div>
-    </main>
+    </div>
+    
 </body>
 
 </html>
