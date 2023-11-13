b40430359304ac711b6f08fef4e8ee2694854ef1
Gabriel Kleber <gabrielkleber54321n@gmail.com>
2023-11-13 17:12:59 -0300

    falta pouco

--- core/views/src/carrinho.php
+++ core/views/src/carrinho.php
@@ -2,20 +2,7 @@
     <div class="carrinho-comp">
         <h2 class="car">Carrinho</h2>
         <input type="hidden" name="email" class="email_u" value="<?= $_SESSION['email']?>">
-        <div class="prod">
-            <abbr title="Remover do Carrinho">
-                <button class="btn-remove">
-                    <i class="bi bi-trash-fill"></i>
-                </button>
-            </abbr>
-            <div class="aparencia-prod">
-                <img src="" alt="" class="img-prod-car">
-                <h3 class="nome-prod"></h3>
-            </div>
-            <div class="desc-prod">
-                <p class="desc-car"></p>
-                <p class="preco"></p>
-            </div>
+        <div class="itens-carrinho">
         </div>
         <div class="finalizar-comp">
             <h3 class="total">Total a pagar: R$0,0</h3>
--- public_html/assets/js/itensCarrinho.js
+++ public_html/assets/js/itensCarrinho.js
@@ -1,6 +1,7 @@
 function itensCarrinho() {
     $(function(e) {
         var email_u = document.querySelector('.email_u').value;
+        var itensCarrinho = document.querySelector('.itens-carrinho');
         $.ajax({
             url: './public_html/itensCarrinho.php',
             method: 'POST',
@@ -8,9 +9,10 @@ function itensCarrinho() {
             dataType: 'json'
         }).done(function(result) {
             if(result[0] == true) {
+                itensCarrinho.innerHTML = ""
                 for (let i = 1; i < result.length; i++) {
                     const element = result[i];
-                    console.log('oi')
+                    itensCarrinho.innerHTML += '<div class="prod"> <abbr title="Remover do Carrinho"> <button class="btn-remove"> <i class="bi bi-trash-fill"></i> </button> </abbr> <div class="aparencia-prod"> <img src="public_html\\assets\\images\\comidas\\'+ element['img'] +'" alt="'+ element['nome'] +'" class="img-prod-car"> <h3 class="nome-prod">'+ element['nome'] +'</h3> </div><div class="desc-prod"><p class="desc-car">'+ element['descricao_saiba_mais'] +'</p> <p class="preco">R$'+ element['preco'].toFixed(2) +'</p> </div> </div>';
                 }
             }
         });
