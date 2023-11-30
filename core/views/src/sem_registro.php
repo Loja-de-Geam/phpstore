342e3bd370b8425d469527971dfd227f6366719d
Nafanael Pereira Alves <126331218+nafanael@users.noreply.github.com>
2023-11-30 19:53:38 -0300

    amen

--- core/views/src/sem_registro.php
+++ core/views/src/sem_registro.php
@@ -1,3 +1,6 @@
-<?php 
-echo 'Sem registros';
+<?php
+$semRegistros = true;
+$mensagem = 'Infelizmente não foi possível encontrar o que você deseja.';
+$semRegistrosClass = 'sem-registros';
+echo '<div class="' . $semRegistrosClass . '">' . $mensagem . '</div>';
 ?>
